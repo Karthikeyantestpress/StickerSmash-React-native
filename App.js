c63886e21f397469bb5291028500a331c4ff94a0
karthikeyan <karthikeyan@testpress.in>
2024-08-20 13:57:57 +0530

    Move image viewer to a seperate folder

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
-
-const PlaceholderImage = require('./assets/images/background-image.png')
+import { StyleSheet, View } from 'react-native';
+import ImageViewer from './components/ImageViewer';
 
 export default function App() {
   return (
     <View style={styles.container}>
         <View style={styles.imageContainer}>
-          <Image source={PlaceholderImage} style={styles.image}/>
+        <ImageViewer/>
         </View>
         <StatusBar style="auto" />
     </View>
@@ -20,13 +19,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#25292e',
     alignItems: 'center',
   },
-  imageContainer:{
-    flex:1,
-    paddingTop:58,
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
   },
-  image: {
-    width: 320,
-    height: 440,
-    borderradius : 18,
-  }
 });
